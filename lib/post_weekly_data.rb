7439cdcd3fbc5d349b5ad64290370c9b07c98988
rhayes <rhayes@virtualconnect.net>
2020-06-25 16:20:49 -0500

    6/25 update

--- lib/post_weekly_data.rb
+++ lib/post_weekly_data.rb
@@ -40,13 +40,17 @@ class PostWeeklyData
     end
     volume.save!
 
+    entry_number = 0
     loop do
       result = find_keyword('Invoice No', row.idx)
       break if (result == false) || !next_row[0].kind_of?(Fixnum)
-      delivery = FuelDelivery.where(:invoice_number => row[0],
-        :week_id => week.id, :delivery_date => row[1]).first_or_create!
-      delivery.monthly_tank_charge_cents = Money.new(100*row[2]).cents
-      delivery.adjustment_cents = Money.new(100*row[3]).cents
+      sequence = week.id.to_s + "-" + entry_number.to_s.rjust(3,'0')
+      delivery = FuelDelivery.find_by_posting_sequence(sequence)
+      delivery = FuelDelivery.new(:posting_sequence => sequence) if delivery.nil?
+      delivery.update_attributes(:invoice_number => row[0],
+        :week_id => week.id, :delivery_date => row[1],
+        :monthly_tank_charge_cents => Money.new(100*row[2]).cents,
+        :adjustment_cents => Money.new(100*row[3]).cents)
       loop do
         break if row[4].nil?
         grade_name = row[4]
@@ -55,6 +59,7 @@ class PostWeeklyData
         next_row
       end
       delivery.save!
+      entry_number += 1
     end
   end
 
@@ -77,32 +82,32 @@ class PostWeeklyData
     end
   end
 
-    def post_transactions
-      return if find_keyword('Date', 0) == false
-      entry_number = 0
-      transaction_types = Transaction.pluck(:category).uniq
-      loop do
-        next_row
-        break if row[0].nil? || !row[0].kind_of?(Date)
-        category = row[2]
-        raise "'#{category}' is invalid category!" unless transaction_types.include?(category)
-        sequence = week.id.to_s + "-" + entry_number.to_s.rjust(3,'0')
-        transaction = Transaction.find_by_posting_sequence(sequence)
-        transaction = Transaction.new(:posting_sequence => sequence) if transaction.nil?
-        transaction.date = row[0]
-        transaction.amount = row[1]
-        transaction.category = category
-        transaction.type_of = ['fuel_sale','rent'].include?(category) ? 'credit' : 'debit'
-        transaction.description = row[3].nil? ? "" : row[3]
-        transaction.check_number = row[4]
-        transaction.tax_year = (row[5] == 0) ? week.tax_year : row[5]
-        transaction.fuel_delivery_id = (row[6] == 0) ? nil : row[6]
-        transaction.week_id = (row[7] == 0) ? week.id : row[7]
-        transaction.balance = 0.0
-        transaction.save!
-        entry_number += 1
-      end
+  def post_transactions
+    return if find_keyword('Date', 0) == false
+    entry_number = 0
+    transaction_types = Transaction.pluck(:category).uniq
+    loop do
+      next_row
+      break if row[0].nil? || !row[0].kind_of?(Date)
+      category = row[2]
+      raise "'#{category}' is invalid category!" unless transaction_types.include?(category)
+      sequence = week.id.to_s + "-" + entry_number.to_s.rjust(3,'0')
+      transaction = Transaction.find_by_posting_sequence(sequence)
+      transaction = Transaction.new(:posting_sequence => sequence) if transaction.nil?
+      transaction.date = row[0]
+      transaction.amount = row[1]
+      transaction.category = category
+      transaction.type_of = ['fuel_sale','rent'].include?(category) ? 'credit' : 'debit'
+      transaction.description = row[3].nil? ? "" : row[3]
+      transaction.check_number = row[4]
+      transaction.tax_year = (row[5] == 0) ? week.tax_year : row[5]
+      transaction.fuel_delivery_id = (row[6] == 0) ? nil : row[6]
+      transaction.week_id = (row[7] == 0) ? week.id : row[7]
+      transaction.balance = 0.0
+      transaction.save!
+      entry_number += 1
     end
+  end
 
   def find_keyword(keyword, starting_row_no)
     for i in starting_row_no..starting_row_no+9
